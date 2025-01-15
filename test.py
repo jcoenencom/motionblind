8c3649e19f1d0318fb9b21d4ddf5b2c59fb764ca
jcoenencom <jpcoenen@gmail.com>
2025-01-15 20:19:15 +0100

    First draft

---
+++ test.py
@@ -0,0 +1,110 @@
+import asyncio
+from motionblinds import MotionGateway
+
+from .. import fhem, generic
+
+
+class test(generic.FhemModule):
+    def __init__(self, logger):
+        super().__init__(logger)
+        self.key = None
+        self.IP = None
+
+
+    # FHEM FUNCTION
+    async def Define(self, hash, args, argsh):
+        await super().Define(hash, args, argsh)
+
+    # define the attributes
+
+        self._attr_list = {
+            "key": {"default": "127.0.0.1"},
+            "IP": {"default": None},
+        }
+
+        await self.set_attr_config(self._attr_list)
+
+    # check the defined attributes in the define command
+        if len(args) < 5:
+            return "Usage: define NAME fhempy test IP"
+
+        self.address = args[3]
+        hash["IP"]= args[3]
+
+        self.logger.info(f"Define test address {self.address}")
+
+        self.key = args[4]
+        
+    
+        self.logger.info(f"Define test key {self.address}")
+
+        set_config = {
+            "mode": {
+                "args": ["mode"],
+                "argsh": ["mode"],
+                "params": {"mode": {"default": "eco", "optional": False}},
+                "options": "eco,comfort",
+            },
+            "desiredTemp": {"args": ["temperature"], "options": "slider,10,1,30"},
+            "holidayMode": {
+                "args": ["endday", "endtime", "temperature"],
+                "params": {
+                    "endday": {"default": "31.12.2030"},
+                    "endtime": {"default": "23:59"},
+                    "temperature": {"default": 21, "format": "int"},
+                },
+            },
+            "on": {
+                "args": ["seconds"],
+                "params": {
+                    "seconds": {"default": 0, "optional": True, "format": "int"}
+                },
+                "help": "Specify seconds as parameter to change to off after X seconds.",
+            },
+            "off": {},
+            "up": {},
+            "down": {},
+        }
+        await self.set_set_config(set_config)
+
+
+        if len(args) > 3:
+            return "Usage: define hello_fhempy fhempy helloworld"
+        
+        await fhem.readingsBeginUpdate(hash)
+        await fhem.readingsBulkUpdateIfChanged(hash, "state", "on")
+        await fhem.readingsEndUpdate(hash, 1)
+        # Attribute function format: set_attr_NAMEOFATTRIBUTE(self, hash)
+        # self._attr_NAMEOFATTRIBUTE contains the new state
+        async def set_attr_IP(self, hash):
+            # attribute was set to self._attr_IP
+            # you can use self._attr_interval already with the new variable
+            pass
+
+        # Set functions in format: set_NAMEOFSETFUNCTION(self, hash, params)
+    async def set_on(self, hash, params):
+        # params contains the keyword which was defined in set_list_conf for "on"
+        # if not provided by the user it will be "" as defined in set_list_conf (default = "" and optional = True)
+        seconds = params["seconds"]
+        if seconds != 0:
+            await fhem.readingsSingleUpdate(hash, "state", "on " + str(seconds), 1)
+        else:
+            await fhem.readingsSingleUpdate(hash, "state", "on", 1)
+
+    async def set_off(self, hash, params):
+        # no params argument here, as set_off doesn't have arguments defined in set_list_conf
+        await fhem.readingsSingleUpdate(hash, "state", "off", 1)
+        self.create_async_task(self.long_running_task())
+        return ""
+
+    async def set_up(self, hash, params):
+        # no params argument here, as set_off doesn't have arguments defined in set_list_conf
+        await fhem.readingsSingleUpdate(hash, "state", "off", 1)
+        self.create_async_task(self.long_running_task())
+        return ""
+
+    async def set_down(self, hash, params):
+        # no params argument here, as set_off doesn't have arguments defined in set_list_conf
+        await fhem.readingsSingleUpdate(hash, "state", "off", 1)
+        self.create_async_task(self.long_running_task())
+        return ""
