4ca5461e03f07c70bd1e9cd54951a6c00ee633f7
jcoenencom <jpcoenen@gmail.com>
2025-01-18 16:22:38 +0100

    Update motionblinds.py
    Added real commands to gateway in live mode

--- motionblinds.py
+++ motionblinds.py
@@ -1,54 +1,77 @@
 import asyncio
-from motionblinds import MotionGateway
+from motionblinds import MotionGateway, MotionBlind
 
 from .. import fhem, generic
 
 
 class motionblinds(generic.FhemModule):
+
+    devtypes = {"02000001":"Gateway", "02000002":"Gateway","10000000":"Standard Blind", "10000001":"Top/Down Bottom/Up", "10000002":"Double Roller"}
+
     def __init__(self, logger):
         super().__init__(logger)
         self.key = None
         self.IP = None
-
+        self.gw = MotionGateway
+        self.blind = MotionBlind
+        self.mac = None
+        self.devType = None
+        self.max_angle = 0
+        self.mode = None
+        return
 
     # FHEM FUNCTION
     async def Define(self, hash, args, argsh):
+    
         await super().Define(hash, args, argsh)
+
+        if len(args) < 6:
+            return "Usage: define NAME fhempy motionblinds IP KEY"
+
 
     # define the attributes
 
-        self._attr_list = {
-            "key": {"default": "127.0.0.1"},
-            "IP": {"default": None},
-        }
-
-        await self.set_attr_config(self._attr_list)
+#        await self.set_attr_config(self._attr_list)
         await self.set_icon("fts_garage_door_30")
         await fhem.CommandAttr(self.hash, self.hash["NAME"] + " devStateIcon up:fts_garage_door_down:down down:fts_garage_door_up:up")
 
     # check the defined attributes in the define command
-        if len(args) < 5:
-            return "Usage: define NAME fhempy test IP"
+    # DEFINE name fhempy motionblinds IP KEY MAC DEVICE_TYPE
 
-        self.address = args[3]
-        hash["IP"]= args[3]
-
-        self.logger.info(f"Define test address {self.address}")
-
+        self.IP = args[3]
+        hash['IP']=self.IP
         self.key = args[4]
-        hash["key"] = args[4]
-        
-    
-        self.logger.info(f"Define test key {self.address}")
+        hash['key']=self.key
+        self.mac = args[5]
+        hash['mac'] = self.mac
+        self.devtype = self.devtypes[args[6]]
+        hash['Device_Type'] =  self.devtype
+
+# define the gateway
+        self.gw = MotionGateway(ip = self.IP, key = self.key)
+        self.blind = MotionBlind(gateway=self.gw, mac=self.mac, device_type=self.devtype)
+# get  the device list
+#        self.gw.GetDeviceList()
+#        self.gw.update()
+#        for blind in self.gw.device_list.values():
+#            self.logger.info(f"Device found"+blind)
 
         set_config = {
             "up": {},
             "down": {},
+            "mode": {
+                "args": ["mode"],
+                "argsh": ["mode"],
+                "params": {"mode": {"default": "live", "optional": False}},
+                "options": "live,sim",
+            },
         }
         await self.set_set_config(set_config)
 
-
-        if len(args) > 3:
+#initial mode is sim
+        await fhem.readingsSingleUpdate(hash, "mode", "sim", 1)
+        self.mode = "sim"
+        if len(args) < 5:
             return "Usage: define brel fhempy test"
         
         await fhem.readingsBeginUpdate(hash)
@@ -66,10 +89,32 @@ class motionblinds(generic.FhemModule):
 
     async def set_up(self, hash, params):
         # no params argument here, as set_up doesn't have arguments defined in set_list_conf
-        await fhem.readingsSingleUpdate(self.hash,"state", "up", 1)
-        return ""
+        if (self.mode == "sim"):
+            await fhem.readingsSingleUpdate(self.hash,"state", "up", 1)
+            return "set sim up done"
+        else:
+            self.gw.GetDeviceList()
+            self.gw.Update()
+            blind = gw.device_list[self.mac]
+            self.blind.Open()
+            await fhem.readingsSingleUpdate(self.hash,"state", "up", 1)
 
+            
     async def set_down(self, hash, params):
         # no params argument here, as set_down doesn't have arguments defined in set_list_conf
-        await fhem.readingsSingleUpdate(self.hash,"state", "down", 1)
-        return ""
+        if (self.mode == "sim"):
+            await fhem.readingsSingleUpdate(self.hash,"state", "down", 1)
+            return "Set sim down done"
+        else:
+            self.gw.GetDeviceList()
+            self.gw.Update()
+            blind = gw.device_list[self.mac]
+            self.blind.Close()
+            await fhem.readingsSingleUpdate(self.hash,"state", "down", 1)
+
+    async def set_mode(self, hash, params):
+        # user can specify mode as mode=eco or just eco as argument
+        # params['mode'] contains the mode provided by user
+        self.mode = params["mode"]
+        await fhem.readingsSingleUpdate(hash, "mode", self.mode, 1)
+ 
