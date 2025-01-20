ccdb31147efc5586d80422610d4dbd4cb81ae3bb
jcoenencom <jpcoenen@gmail.com>
2025-01-20 17:49:36 +0100

    set_readings

--- motionblinds.py
+++ motionblinds.py
@@ -144,7 +144,7 @@ class motionblinds(generic.FhemModule):
             blind = self.gw.device_list[self.mac]
             self.blind.Open()
             await fhem.readingsSingleUpdate(self.hash,"state", "up", 1)
-        self.__set_readings()
+        self.set_readings()
             
     async def set_down(self, hash, params):
         # no params argument here, as set_down doesn't have arguments defined in set_list_conf
@@ -159,8 +159,8 @@ class motionblinds(generic.FhemModule):
             self.blind.Update()
 
             await fhem.readingsSingleUpdate(self.hash,"state", "down", 1)
-        self.__set_readings()
-        
+        self.set_readings()
+
     async def set_mode(self, hash, params):
         # user can specify mode as mode=eco or just eco as argument
         # params['mode'] contains the mode provided by user
